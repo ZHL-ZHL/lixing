345413d6d2ab614511a99544672800c1365394bf
ZHL-ZHL <zhl5632@163.com>
2020-06-23 18:13:22 +0800

    修改地址

--- utils/host.js
+++ utils/host.js
@@ -4,11 +4,11 @@ export default {
   //  host: 'http://192.168.0.102:7070', 
   //  host: 'http://192.168.0.101:7070', 
   //  host: 'http://192.168.0.109:7070',
-  host: 'http://39.100.147.189:7090',
+  host: 'https://ysc.hzxxtech.com/api',
   // <---正式服务地址---> 
   // host:'https://zlxcx.sumirafamily.com/api',
-  imgURL: 'http://39.100.147.189:7090/renren-fast',
+  imgURL: 'https://ysc.hzxxtech.com/renren-fast',
   host2: 'http://39.100.156.89/api',
-  imghost: "http://39.100.147.189:7090/renren-fast",
+  imghost: "https://ysc.hzxxtech.com/renren-fast",
   // <---正式服务地址--->
 }
